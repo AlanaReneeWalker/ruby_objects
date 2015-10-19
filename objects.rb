17c65961edc48b8064a09a53629dee4488b6a0db
Alana Walker <pwd>
2015-10-19 16:03:45 -0400

    updaye #3

--- objects.rb
+++ objects.rb
@@ -1,20 +1,20 @@
 class Dog
-		attr_accessor :fur_color
-		attr_accessor :has_legs
-		attr_accessor :can_bark
-
-
-	def has_fur
-		has_fur = "covered in fur"
-		has_legs
-		sound = "woof"
-	end
+		attr_accessor :fur
+		attr_accessor :legs
+		attr_accessor :sound
+		attr_accessor :temperment
 end
 
 class Husky < dog
+		attr_accessor :fur
+		attr_accessor :legs
+		attr_accessor :sound
 		attr_accessor :temperment
-	
+		
 	def initialize
+		fur = "thick"
+		legs = "four"
+		sound = "bark"
 		temperment = "independent"
 	end
 end
